6ca17e674f99c2edb75248ab9e94d6a5d7a34476
Bart de Goede <bart@scribd.com>
2020-09-09 00:08:15 -0700

    Docstring for @timing

--- search/timing.py
+++ search/timing.py
@@ -1,6 +1,21 @@
 import time
 
 def timing(method):
+    """
+    Quick and dirty decorator to time functions: it will record the time when
+    it's calling a function, record the time when it returns and compute the
+    difference. There'll be some overhead, so it's not very precise, but'll
+    suffice to illustrate the examples in the accompanying blog post.
+
+    @timing
+    def snore():
+        print('zzzzz')
+        time.sleep(5)
+
+    snore()
+    zzzzz
+    snore took 5.0011749267578125 seconds
+    """
     def timed(*args, **kwargs):
         start = time.time()
         result = method(*args, **kwargs)
